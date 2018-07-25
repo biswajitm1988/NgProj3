e777c037b7f4fed840fc9d3759db408cb1f1bf90
Biswajit Mohapatra <Biswajit.Mohapatra@rci.com>
2018-07-25 18:28:10 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostGridComponent } from './service/post-grid/post-grid.component';
+import { PostDetailsComponent } from './service/post-details/post-details.component';
+import { PostFormComponent } from './service/post-form/post-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostGridComponent,
+    PostDetailsComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule
