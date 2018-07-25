53a4dd81f0f7ad497b229eb7159302a491fa6382
Biswajit Mohapatra <biswajit1988@gmail.com>
2018-07-25 19:56:52 -0400

    updates

--- src/app/post-grid/post-grid.component.ts
+++ src/app/post-grid/post-grid.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../model/post';
 import { PostService } from '../service/post.service';
+import { Observable } from '../../../node_modules/rxjs';
+import { map } from '../../../node_modules/rxjs/operators';
 
 @Component({
   selector: 'app-post-grid',
@@ -17,6 +19,11 @@ export class PostGridComponent implements OnInit {
     this.postService.listPost().subscribe(
       data => {this.posts=data;this.errMsg=null;console.log(data);},
       error => {this.errMsg="Failed to invoke service";console.log(error)}
+    )
+  }
+  getPost(id:number):Observable<Post>{
+    return this.http.get(this.getIdSpecificUrl(id)).pipe(
+      map(response => response.json())
     )
   }
 
--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '../../../node_modules/@angular/http';
+import { Http, Response } from '../../../node_modules/@angular/http';
 import { Observable } from '../../../node_modules/rxjs';
 import { Post } from '../model/post';
 import { map } from 'rxjs/operators';
@@ -17,6 +17,34 @@ export class PostService {
   listPost(): Observable<Post[]>{
     return this.http.get(this.baseUrl).pipe(
       map(response => response.json())
+    );
+  }
+
+  getIdSpecificUrl(id){
+    return this.baseUrl+"/"+id;
+  }
+
+  getPost(id:number):Observable<Post>{
+    return this.http.get(this.getIdSpecificUrl(id)).pipe(
+      map(response => response.json())
+    );
+  }
+
+  addPost(post:Post):Observable<Post>{
+    return this.http.put(this.baseUrl,JSON.stringify(post)).pipe(
+      map(response => response.json())
+    );
+  }
+
+  savePost(post:Post):Observable<Post>{
+    return this.http.put(this.getIdSpecificUrl(post.id),JSON.stringify(post)).pipe(
+      map(response => response.json())
+    );
+  }
+
+  deletePost(id:number):Observable<Response>{
+    return this.http.delete(this.getIdSpecificUrl(id)).pipe(
+      map(response => response)
     );
   }
 }
